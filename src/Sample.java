7888f5be1a3aa2c841c689bdf85a8cc5fb0b8aca
gye7072 <gye7072@bths.edu>
2022-09-20 16:39:13 -0400

    Added print statement for food

--- src/Sample.java
+++ src/Sample.java
@@ -2,6 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Gary Ye");
         System.out.println("Parasite");
+        System.out.println("ice Cream");
     }
 }
 
